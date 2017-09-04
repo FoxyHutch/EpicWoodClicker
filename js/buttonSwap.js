3670c80004e0557fe899eba20d3354c89a336a46
Kester <kester.lotz@googlemail.com>
2017-09-04 17:26:09 +0200

    Swap between buttons

--- js/buttonSwap.js
+++ js/buttonSwap.js
@@ -1,28 +1,49 @@
 $(document).ready(function() {
-  var clickerImages = ["img/PixelHolzZwei.png", "img/PixelStein.png",
-    "img/PixelNahrung.png", "img/PixelGold.png"
-  ];
-  var clickerIDs = ["holz-btn", "stein-btn", "nahrung-btn", "gold-btn"]
   var currButton = 0;
 
-  $('#next').on('click', function functionName() {
-      var clickerButton = $("div.clicker-container").children("input");
-      if (currButton == 3) {
-        currButton = 3;
-      } else {
+  /* Hides the current Button and displays next one */
+  $('#next').on('click', function getNext() {
+    switch (currButton) {
+      case 0:
+        $("#holz-btn").toggle();
+        $("#stein-btn").toggle();
         currButton += 1;
-      };
-      clickerButton.attr("src", clickerImages[currButton] );
-      clickerButton.attr("id", clickerIDs[currButton]);
+        break;
+      case 1:
+        $("#stein-btn").toggle();
+        $("#nahrung-btn").toggle();
+        currButton += 1;
+        break;
+      case 2:
+        $("#nahrung-btn").toggle();
+        $("#gold-btn").toggle();
+        currButton += 1;
+        break;
+      default:
+        break;
+    }
   });
-  $('#prev').on('click', getPrev(clickerImages,clickerIDs,  currButton));
+
+  /* Hides current Button and displays previous one */
+  $('#prev').on('click', function getPrev() {
+    switch (currButton) {
+      case 1:
+        $("#stein-btn").toggle();
+        $("#holz-btn").toggle();
+        currButton -= 1;
+        break;
+      case 2:
+        $("#nahrung-btn").toggle();
+        $("#stein-btn").toggle();
+        currButton -= 1;
+        break;
+      case 3:
+        $("#gold-btn").toggle();
+        $("#nahrung-btn").toggle();
+        currButton -= 1;
+        break;
+      default:
+        break;
+    }
+  });
 });
-
-function getNext(images, ids,  curr) {
-
-};
-
-
-function getPrev(images, ids,  curr) {
-
-}
